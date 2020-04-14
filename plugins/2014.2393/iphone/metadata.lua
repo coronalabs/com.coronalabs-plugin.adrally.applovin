fc3a2a15a98c961ac27126b9a6e06d4b012ccd5c
Vlad Shcherban <vlad@coronalabs.com>
2020-04-13 23:09:54 -0400

    Adding binaries

---
+++ plugins/2014.2393/iphone/metadata.lua
@@ -0,0 +1,12 @@
+local metadata =
+{
+	plugin =
+	{
+		format = 'staticLibrary',
+		staticLibs = { 'FuseAdapterAppLovin' },
+		frameworks = {},
+		frameworksOptional = {},
+	},
+}
+
+return metadata
